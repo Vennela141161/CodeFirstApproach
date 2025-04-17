64e7f5b1b96b91d3a4052dc137490cbd731b37c6
naden <naden@VENNELA>
2025-04-17 15:47:46 +0530

    Add project files.

---
+++ CodeFirstApproach/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirstApproach.Models
+{
+    public class Student
+    {
+        [Key]
+        public int? Id { get; set; }
+        [Column("StudentName",TypeName="varchar(50)")]
+        public string Name { get; set; }
+        [Column("StrudentEmail",TypeName ="varchar(50)")]
+        public string Email { get; set; }
+        public int Age { get; set; }
+
+    }
+}
